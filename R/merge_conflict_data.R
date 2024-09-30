82712fef8eff4cf52b7d80ad2f4faa8907d4768c
MHelmann <maksim.helmann@gmail.com>
2024-09-30 11:47:06 -0400

    change merge to left join and reorder the columns

--- R/merge_conflict_data.R
+++ R/merge_conflict_data.R
@@ -16,24 +16,29 @@ source(here("R","clean_disaster.R"))
 source(here("R","create_mortality.R"))
 source(here("R","binary_armed_conflict.R"))
 
-
-# Change column name of disaster dataset from "Year" -> "year"
-colnames(disaster_clean)[1] <- "year"
-
 # Add all data frames into list
-all_datasets <- list(conflict_binary, disaster_clean, mortality_full, cov_data)
+all_datasets <- list(conflict_binary, disaster_clean, mortality_full)
 # Merge all the data frames in list
-final_mortality_data <- reduce(all_datasets, full_join, by = c("year", "ISO"))
+final_data_ <- reduce(all_datasets, full_join, by = c("year", "ISO"))
+
+final_mort_data <- cov_data |>
+  left_join(final_data_, by = c('ISO', 'year'))
 # Check to see if there are 20 rows of data for each country.
-dim(final_mortality_data)
+dim(final_mort_data)
 
 # replace NAs with 0's for variables: armconf1, drougt, earthquake and totaldeath
-final_mortality_data <- final_mortality_data |> 
+final_mort_data <- final_mort_data |> 
   mutate(armconf1 = replace_na(armconf1, 0),
          drought = replace_na(drought, 0),
          earthquake = replace_na(earthquake, 0),
-         totaldeath = replace_na(totaldeath, 0))
+         totdeath = replace_na(totdeath, 0))
+
+# Reorder columns
+final_mort_data <- final_mort_data[ , c("country_name", "ISO", "region", "year", "gdp1000", "OECD", "OECD2023", 
+                                        "popdens", "urban", "agedep", "male_edu", "temp", "rainfall1000",
+                                        "totdeath", "armconf1", "matmor", "infmor", "neomor", "un5mor",
+                                        "drought", "earthquake")]
 
 # Save final dataset
-write.csv(final_mortality_data, file = here("data", "analytical", "final_data.csv"), row.names = FALSE)
+write.csv(final_mort_data, file = here("data", "analytical", "final_mort_data.csv"), row.names = FALSE)
 
